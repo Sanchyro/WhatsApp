7970133fd7df295963a152b31a4c170dd3d1547f
Sanchyro Eijkemans <sanchyro@live.nl>
2021-12-09 21:36:26 +0100

    Added Call Screen

---
+++ lib/pages/call_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CallsScreen extends StatelessWidget {
+  const CallsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Calls",
+        style: TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
